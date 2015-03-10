89f55f794673bc15bc96c9cdafd6b51572d1731d
cps543 <sc.valencia606@gmail.com>
2015-03-10 11:59:47 -0500

    First tier of authentication

--- app/controllers/Aura.java
+++ app/controllers/Aura.java
@@ -2,40 +2,65 @@ package controllers;
 
 import actions.CorsComposition;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import models.Doctor;
 import play.*;
 import play.data.Form;
 import play.data.Form.*;
+import play.db.ebean.Model;
+import play.libs.Json;
 import play.mvc.*;
 
-import views.html.index;
-import views.html.login;
-import views.html.search;
-import views.html.info;
+import views.html.*;
 
 @CorsComposition.Cors
 public class Aura extends Controller {
 
 
     public static Result index() {
-        return unauthorized("you must log in to use this function");
+        java.util.Set<String> tokenP = session().keySet();
+
+        if(tokenP.isEmpty()) {
+            return ok(login.render(""));
+        }
+        else {
+            java.util.List<String> tokenId=new java.util.ArrayList<String>(tokenP);
+            String id = tokenId.get(0);
+            String token = session().get(id);
+            System.out.println("id:"+id+"-token:"+token);
+            return ok(search.render(id+"--TOKEN--"+token));
+        }
 
     }
 
+    public static Result unauthorizedAccess() {
+
+        return ok(unauthorizedAccess.render(""));
+    }
+
     public static Result token(String path) {
+
         return ok("");
     }
 
     public static Result login() {
+
         return ok(login.render(""));
     }
 
     public static Result search(String session) {
-        return ok(
-                search.render(session));
+
+        String token = session().get(session);
+        if (token != null) {
+            //TODO verificar token
+            return ok(search.render(session));
+        } else {
+            return ok(unauthorizedAccess.render(""));
+        }
     }
 
     public static Result info(String session) {
+        session().clear();
         return ok(
                 info.render(session));
     }
