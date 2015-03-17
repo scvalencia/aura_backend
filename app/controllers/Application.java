46b0f990146df6e79dadc4a55b8d17780304f18d
cps543 <sc.valencia606@gmail.com>
2015-03-16 23:28:55 -0500

    Cors action implemented for Heroku deploying

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,20 @@
 package controllers;
 
+import actions.CorsComposition;
 import play.*;
 import play.mvc.*;
 
 import views.html.*;
 
+@CorsComposition.Cors
 public class Application extends Controller {
 
     public static Result index() {
         return ok(index.render("Your new application is ready."));
+    }
+
+    public static Result token(String path) {
+        return ok("");
     }
 
 }
