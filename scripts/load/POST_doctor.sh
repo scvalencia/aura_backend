905904769bd1cf0f25ca151587b5cbe2eb634d97
cps543 <sc.valencia606@gmail.com>
2015-03-18 11:23:16 -0500

    Automated scripts for POSTING data into the remote or local app

---
+++ scripts/load/POST_doctor.sh
@@ -0,0 +1,32 @@
+curl -v -H "Content-type: application/json" -X POST -d '{"discipline": 8, "name": "Weiss McEvoy", "gender": 0, "id": 16437683164, "date": "1946-11-27", "password": "POZXG4LK", "email": "wmcevoy642@gmail.com"}' http://localhost:9000/api/doctor
+curl -v -H "Content-type: application/json" -X POST -d '{"link": "www.linkedin.com/16437683164"}' http://localhost:9000/api/doctor/16437683164/link
+curl -v -H "Content-type: application/json" -X POST -d '{"discipline": 6, "name": "Sattar Grotrian", "gender": 1, "id": 57537837463, "date": "1989-10-21", "password": "759SAZMH", "email": "sgrotrian989@gmail.com"}' http://localhost:9000/api/doctor
+curl -v -H "Content-type: application/json" -X POST -d '{"link": "www.linkedin.com/57537837463"}' http://localhost:9000/api/doctor/57537837463/link
+curl -v -H "Content-type: application/json" -X POST -d '{"discipline": 8, "name": "Wolfman Aly", "gender": 0, "id": 23089859106, "date": "1987-08-22", "password": "994BD79E", "email": "waly986@gmail.com"}' http://localhost:9000/api/doctor
+curl -v -H "Content-type: application/json" -X POST -d '{"link": "www.linkedin.com/23089859106"}' http://localhost:9000/api/doctor/23089859106/link
+curl -v -H "Content-type: application/json" -X POST -d '{"discipline": 1, "name": "Saunders Hatfield", "gender": 0, "id": 40921064651, "date": "1980-02-14", "password": "V4A3W5Y4", "email": "shatfield812@gmail.com"}' http://localhost:9000/api/doctor
+curl -v -H "Content-type: application/json" -X POST -d '{"link": "www.linkedin.com/40921064651"}' http://localhost:9000/api/doctor/40921064651/link
+curl -v -H "Content-type: application/json" -X POST -d '{"discipline": 5, "name": "Jonghun Johannesson", "gender": 0, "id": 47124456427, "date": "1939-02-21", "password": "PJWTV19W", "email": "jjohannesson856@gmail.com"}' http://localhost:9000/api/doctor
+curl -v -H "Content-type: application/json" -X POST -d '{"link": "www.linkedin.com/47124456427"}' http://localhost:9000/api/doctor/47124456427/link
+curl -v -H "Content-type: application/json" -X POST -d '{"discipline": 0, "name": "Phuc Hoyt", "gender": 1, "id": 91865671109, "date": "1967-05-16", "password": "QILIPB52", "email": "phoyt517@gmail.com"}' http://localhost:9000/api/doctor
+curl -v -H "Content-type: application/json" -X POST -d '{"link": "www.linkedin.com/91865671109"}' http://localhost:9000/api/doctor/91865671109/link
+curl -v -H "Content-type: application/json" -X POST -d '{"discipline": 2, "name": "Gebhardt Pittman", "gender": 0, "id": 59090502709, "date": "1998-04-12", "password": "GSH9F0IY", "email": "gpittman682@gmail.com"}' http://localhost:9000/api/doctor
+curl -v -H "Content-type: application/json" -X POST -d '{"link": "www.linkedin.com/59090502709"}' http://localhost:9000/api/doctor/59090502709/link
+curl -v -H "Content-type: application/json" -X POST -d '{"discipline": 6, "name": "Wiltz Garside", "gender": 1, "id": 20854185310, "date": "1971-01-08", "password": "FOBSDBWO", "email": "wgarside834@gmail.com"}' http://localhost:9000/api/doctor
+curl -v -H "Content-type: application/json" -X POST -d '{"link": "www.linkedin.com/20854185310"}' http://localhost:9000/api/doctor/20854185310/link
+curl -v -H "Content-type: application/json" -X POST -d '{"discipline": 6, "name": "Hellyer Casto", "gender": 0, "id": 75768763344, "date": "1955-01-04", "password": "VRPS096W", "email": "hcasto719@gmail.com"}' http://localhost:9000/api/doctor
+curl -v -H "Content-type: application/json" -X POST -d '{"link": "www.linkedin.com/75768763344"}' http://localhost:9000/api/doctor/75768763344/link
+curl -v -H "Content-type: application/json" -X POST -d '{"discipline": 7, "name": "Biersach Boezi", "gender": 0, "id": 34252667014, "date": "1962-07-04", "password": "TYCSR9RQ", "email": "bboezi816@gmail.com"}' http://localhost:9000/api/doctor
+curl -v -H "Content-type: application/json" -X POST -d '{"link": "www.linkedin.com/34252667014"}' http://localhost:9000/api/doctor/34252667014/link
+curl -v -H "Content-type: application/json" -X POST -d '{"discipline": 8, "name": "Antons McIlroy", "gender": 0, "id": 16996800567, "date": "1966-02-16", "password": "TOXDZMUY", "email": "amcilroy186@gmail.com"}' http://localhost:9000/api/doctor
+curl -v -H "Content-type: application/json" -X POST -d '{"link": "www.linkedin.com/16996800567"}' http://localhost:9000/api/doctor/16996800567/link
+curl -v -H "Content-type: application/json" -X POST -d '{"discipline": 8, "name": "Reimburs Cortese", "gender": 0, "id": 18369153915, "date": "1987-09-19", "password": "RKCILQ66", "email": "rcortese255@gmail.com"}' http://localhost:9000/api/doctor
+curl -v -H "Content-type: application/json" -X POST -d '{"link": "www.linkedin.com/18369153915"}' http://localhost:9000/api/doctor/18369153915/link
+curl -v -H "Content-type: application/json" -X POST -d '{"discipline": 1, "name": "Golia Spartichino", "gender": 0, "id": 73231220938, "date": "1972-05-26", "password": "QF20DQCS", "email": "gspartichino453@gmail.com"}' http://localhost:9000/api/doctor
+curl -v -H "Content-type: application/json" -X POST -d '{"link": "www.linkedin.com/73231220938"}' http://localhost:9000/api/doctor/73231220938/link
+curl -v -H "Content-type: application/json" -X POST -d '{"discipline": 5, "name": "Stites Gutierrez-Jones", "gender": 0, "id": 76126076590, "date": "1943-02-23", "password": "DR2G81PJ", "email": "sgutierrez-jones862@gmail.com"}' http://localhost:9000/api/doctor
+curl -v -H "Content-type: application/json" -X POST -d '{"link": "www.linkedin.com/76126076590"}' http://localhost:9000/api/doctor/76126076590/link
+curl -v -H "Content-type: application/json" -X POST -d '{"discipline": 4, "name": "Issam Shayegan", "gender": 1, "id": 32725417552, "date": "1952-02-05", "password": "JYKO0GAA", "email": "ishayegan104@gmail.com"}' http://localhost:9000/api/doctor
+curl -v -H "Content-type: application/json" -X POST -d '{"link": "www.linkedin.com/32725417552"}' http://localhost:9000/api/doctor/32725417552/link
+curl -v -H "Content-type: application/json" -X POST -d '{"discipline": 3, "name": "Aggregat Guberman", "gender": 0, "id": 63415293891, "date": "1933-04-15", "password": "9OIP5KB5", "email": "aguberman475@gmail.com"}' http://localhost:9000/api/doctor
+curl -v -H "Content-type: application/json" -X POST -d '{"link": "www.linkedin.com/63415293891"}' http://localhost:9000/api/doctor/63415293891/link
