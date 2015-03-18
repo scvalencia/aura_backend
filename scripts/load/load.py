a262ea99d8a8aa2889069d221cee55b4be0a8423
cps543 <sc.valencia606@gmail.com>
2015-03-18 11:22:13 -0500

    A python scripts for generate sh script to populate local or remote database

--- scripts/load/load.py
+++ scripts/load/load.py
@@ -1,6 +1,8 @@
+import itertools
 import sqlite3
 import random
 import string
+import json
 import sys
 
 '''
@@ -19,6 +21,56 @@ import sys
 
 db_name = 'load.db'
 connection = sqlite3.connect(db_name)
+
+remote_url = 'https://aura-prod.herokuapp.com/'
+local_url = 'http://localhost:9000/'
+
+def print_info():
+	chains = []
+	chains.append('\n\nload: A tool for database administration at aura (internal usage).\n')
+	chains.append('\n')
+
+	chains.append('\tload is a tool to generate consistent input data to insert data\n')
+	chains.append('\tinto the aura databases (production, prototype, development). load\n')
+	chains.append('\ttakes as arguments an entity to generate data from, a place to bulk\n')
+	chains.append('\tdata into (production, development, prototyping); an a max bound\n')
+	chains.append('\tto count the rows to generate.\n')
+
+	chains.append('\n')
+
+	chains.append('\tIf the database is the prototype database (sqlite), load, is capable\n')
+	chains.append('\tof insert data directly to the database. If the database is the\n')
+	chains.append('\tproduction or development databse, located in Heroku, ans localhost\n')
+	chains.append('\trespectively, load should generate shell scripts to POST data to the services\n')
+	chains.append('\tlayer of the app.\n')
+
+	chains.append('\n')
+
+	chains.append('\tload is a custom command line program, if the entity, the protocol or the\n')
+	chains.append('\tdatabase changes, load should be uploaded.\n')
+
+	chains.append('\n')
+
+	chains.append('SYNTAX:\n\n')
+
+	chains.append('\tpython load.py -h\n')
+	chains.append('\t\tprints this message\n\n')
+
+	chains.append('\tpython load.py (-p | -d | -e) (-l | -r) (%d*) \n')
+	chains.append('\t\tGenerates shell scripts for the given entity, at localhost or remote stage.\n')
+	chains.append('\t\tThe shell script is for POSTING objects to the app using curl.\n')
+	chains.append('\t\t-p is for patient, -d is for doctor, -e is for episodes. If the -p, or -d\n')
+	chains.append('\t\tflags are picked, %d is an integer to show how many objects do you want to send.\n')
+	chains.append('\t\tOtherwise, if -e is selected, a script would be generated to POST at maximum %d\n')
+	chains.append('\t\tepisodes for each patient in the respective database.\n\n')
+
+	chains.append('\tpython load.py -sc (-p | -d) (%d*) \n')
+	chains.append('\t\tLoads to sqlite3 the tables doctor (-d), or patient (-p), with %d* rows.\n\n')
+
+	chains.append('\tpython load.py -sl (-p | -d) \n')
+	chains.append('\t\tRemoves the data in sqlite3 from the table doctor (-d), or patient (-p).\n\n')
+
+	print ''.join(chains)
 
 def create_tables():
 	qry = open('load.sql', 'r').read()
@@ -131,62 +183,259 @@ def clear_localSQLite_data():
 	connection.commit()
 	c.close()
 
+def generate_curl_patients(bound, local):
+	
+	class Patient(object):
 
-def print_info():
-	chains = []
-	chains.append('\n\nload: A tool for database administration at aura (internal usage).\n')
-	chains.append('\n')
+		def __init__(self, arg_id, name, email, password, date, gender):
 
-	chains.append('\tload is a tool to generate consistent input data to insert data\n')
-	chains.append('\tinto the aura databases (production, prototype, development). load\n')
-	chains.append('\ttakes as arguments an entity to generate data from, a place to bulk\n')
-	chains.append('\tdata into (production, development, prototyping); an a max bound\n')
-	chains.append('\tto count the rows to generate.\n')
+			self.id = arg_id
+			self.name = name
+			self.email = email
+			self.password = password
+			self.date = date
+			self.gender = gender
 
-	chains.append('\n')
+		def get_json(self):
+			return json.dumps(self, default=lambda o: o.__dict__)
 
-	chains.append('\tIf the database is the prototype database (sqlite), load, is capable\n')
-	chains.append('\tof insert data directly to the database. If the database is the\n')
-	chains.append('\tproduction or development databse, located in Heroku, ans localhost\n')
-	chains.append('\trespectively, load should generate shell scripts to POST data to the services\n')
-	chains.append('\tlayer of the app.\n')
+	c = connection.cursor()
+	c.execute('SELECT * FROM patient')
+	patients = [_ for _ in c.fetchall()]
 
-	chains.append('\n')
+	if bound > len(patients):
+		print 'Not enough patients to write the file: execute python load.py -sc -p ' + str(bound - len(patients))
+		print 'Then, execute python load.py -p (-l | -r) ' + str(bound)
+		return
 
-	chains.append('\tload is a custom command line program, if the entity, the protocol or the\n')
-	chains.append('\tdatabase changes, load should be uploaded.\n')
+	random.shuffle(patients)
 
-	chains.append('\n')
+	used_ids = open('patients.txt', 'a')
+	post_file = open('POST_patient.sh', 'w')
 
-	chains.append('SYNTAX:\n\n')
+	i = 0
+	url = local_url if local else remote_url
+	service = 'api/patient'
 
-	chains.append('\tpython load.py -h\n')
-	chains.append('\t\tprints this message\n\n')
+	for itm in patients:
 
-	chains.append('\tpython load.py (-p | -d | -e) (-l | -r) (%d*) \n')
-	chains.append('\t\tGenerates shell scripts for the given entity, at localhost or remote stage.\n')
-	chains.append('\t\tThe shell script is for POSTING objects to the app using curl.\n')
-	chains.append('\t\t-p is for patient, -d is for doctor, -e is for episodes. If the -p, or -d\n')
-	chains.append('\t\tflags are picked, %d is an integer to show how many objects do you want to send.\n')
-	chains.append('\t\tOtherwise, if -e is selected, a script would be generated to POST at maximum %d\n')
-	chains.append('\t\tepisodes for each patient in the respective database.\n\n')
+		itm_id = itm[0]
+		itm_name = str(itm[1])
+		itm_pass = str(itm[2])
+		itm_date = str(itm[3])
+		itm_mail = str(itm[4])
+		itm_gender = str(itm[5])
 
-	chains.append('\tpython load.py -sc (-p | -d) (%d*) \n')
-	chains.append('\t\tLoads to sqlite3 the tables doctor (-d), or patient (-p), with %d* rows.\n\n')
+		patient_object = Patient(itm_id, itm_name, itm_mail, itm_pass, itm_date, itm_gender)
 
-	chains.append('\tpython load.py -sl (-p | -d) \n')
-	chains.append('\t\tRemoves the data in sqlite3 from the table doctor (-d), or patient (-p).\n\n')
+		line = 'curl -v -H "Content-type: application/json" -X POST -d '
+		line += "'" + patient_object.get_json() + "'" + ' '
+		line += url + service
+		line += '\n'
 
-	print ''.join(chains)
+		post_file.write(line)
+		used_ids.write(str(itm_id) + '\n')
 
-def generate_curl_patients():
-	pass
+		if i == bound:
+			break
 
-def generate_curl_doctors():
-	pass
+		i += 1
 
-def generate_curl_episodes():
-	pass
+	c.close()			
+
+def generate_curl_doctors(bound, local):
+	
+	class Doctor(object):
+
+		def __init__(self, arg_id, gender, name, email, date, password, discipline):
+
+			self.id = arg_id
+			self.gender = gender
+			self.name = name
+			self.email = email
+			self.date = date
+			self.password = password
+			self.discipline = discipline
+
+		def get_json(self):
+			return json.dumps(self, default=lambda o: o.__dict__)
+
+	c = connection.cursor()
+	c.execute('SELECT * FROM doctor')
+	doctors = [_ for _ in c.fetchall()]
+
+	if bound > len(doctors):
+		print 'Not enough doctors to write the file: execute python load.py -sc -d ' + str(bound - len(doctors))
+		print 'Then, execute python load.py -d (-l | -r) ' + str(bound)
+		return
+
+	random.shuffle(doctors)
+
+	post_file = open('POST_doctor.sh', 'w')
+
+	i = 0
+	url = local_url if local else remote_url
+	service = 'api/doctor'
+
+	for itm in doctors:
+
+		d = Doctor(itm[0], itm[1], itm[2], itm[3], itm[4], itm[5], itm[6])
+		link = itm[7]
+
+		line = 'curl -v -H "Content-type: application/json" -X POST -d '
+		line += "'" + d.get_json() + "'" + ' '
+		line += url + service
+		line += '\n'
+
+		new_line = 'curl -v -H "Content-type: application/json" -X POST -d '
+		new_line += "'" + json.dumps({"link" : str(link)}) + "'" + ' '
+		new_line += url + service + '/' + str(itm[0]) + '/' + 'link' + '\n' 
+
+		post_file.write(line)
+		post_file.write(new_line)
+
+		if i == bound:
+			break
+
+		i += 1
+		
+
+	c.close()
+
+def generate_curl_episodes(bound, local):
+	# Get patients from file, if file don't exist inform, otherwise, execute
+
+	foods = ['Frijol', 'Chocolate', 'Cafe', 'Arroz', 'Sopa', 'Dulces', 'Arequipe',
+	'Gaseosa', 'Te', 'Carne', 'Pescado', 'Pollo']
+
+	medicines = ['Dolex', 'Morfina', 'Loratadina', 'Acetaminofen']
+
+	class Episode(object):
+
+		def __init__(self, url, intensity, sleep, regular, location, stress):
+			self.urlId = url
+			self.intensity = intensity
+			self.sleepHours = sleep
+			self.regularSleep = regular
+			self.location = location
+			self.stress = stress
+			self.symptoms = []
+			self.foods = []
+			self.sports = []
+			self.medicines = []
+			
+		def add_symtom(self, s):
+			self.symptoms.append(s)
+
+		def add_food(self, f):
+			self.foods.append(f)
+
+		def add_sport(self, s):
+			self.sports.append(s)
+
+		def add_medicine(self, m):
+			self.medicines.append(m)
+
+		def get_json(self):
+			return json.dumps(self, default=lambda o: o.__dict__)
+
+	class Symptom(object):
+
+		def __init__(self):
+			self.symptom = random.choice(range(1, 11))
+
+	class Food(object):
+
+		def __init__(self):
+			self.name = random.choice(foods)
+			self.quentity = random.choice(range(1, 11))
+
+	class Medicine(object):
+
+			def __init__(self):
+				self.name = random.choice(medicines)
+				self.hoursAgo = random.choice(range(0, 13))
+
+	class Sport(object):
+
+		def __init__(self):
+			self.description = random.choice(range(1, 6))
+			self.intensity = random.choice(range(1, 11))
+			self.place = random.choice(range(1, 6))
+			self.climate = random.choice(range(1, 6))
+			self.hydration = random.choice([True, False])
+
+
+	patients = set([i.strip() for i in open('patients.txt', 'r').readlines()])
+	patients = list(patients)
+
+	post_file = open('POST_episode.sh', 'w')
+	url = local_url if local else remote_url
+	service = lambda x : 'api/patient/' + str(x) + '/episode' 
+
+	for patient in patients:
+
+		max_bound = random.choice(range(bound + 1))
+		i = 0
+		while i <= max_bound:
+
+			e = None
+			
+			soundCloud = random.choice([True, False])
+
+			if soundCloud:
+				sound_url = ''.join(random.choice(string.digits) for _ in range(8))
+				intensity = 10
+				sleep = ''
+				regular = ''
+				location = ''
+				stress = ''
+
+				e = Episode(sound_url, intensity, sleep, regular, location, stress)
+
+			else:
+				sound_url = ''
+				intensity = random.choice(range(1, 11))
+				sleep = random.choice(range(1, 13))
+				regular = random.choice([True, False])
+				location = random.choice(range(1, 6))
+				stress = random.choice([True, False])
+
+				e = Episode(sound_url, intensity, sleep, regular, location, stress)
+
+				num_symptoms = random.choice(range(10))
+				num_foods = random.choice(range(10))
+				num_medicines = random.choice(range(10))
+				num_sports = random.choice(range(10))
+
+				j = 0
+				while j <= num_symptoms:
+					e.add_symtom(Symptom())
+					j += 1
+
+				j = 0
+				while j <= num_foods:
+					e.add_food(Food())
+					j += 1
+
+				j = 0
+				while j <= num_medicines:
+					e.add_medicine(Medicine())
+					j += 1
+
+				j = 0
+				while j <= num_sports:
+					e.add_sport(Sport())
+					j += 1
+
+			line = 'curl -v -H "Content-type: application/json" -X POST -d '
+			line += "'" + e.get_json() + "'" + ' '
+			line += url + service(patient)
+			line += '\n'
+
+			post_file.write(line)
+
+			i += 1
 
 def main():
 
@@ -195,12 +444,14 @@ def main():
 	if len(args) == 1:
 		if args[0] == '-h':
 			print_info()
+
 		else:
 			print 'Wrong usage. manual at: python load.py -h'
 
 	elif len(args) == 2:
 		if args[0] == '-sl' and args[1] in ['-d', '-p']:
 			clear_localSQLite_data()
+
 		else:
 			print 'Wrong usage. manual at: python load.py -h'
 
@@ -212,7 +463,24 @@ def main():
 			load_localSQLite_data(entity, bound)
 
 		elif args[0] in ['-d', '-p', '-e'] and args[1] in ['-l', '-r'] and args[2].isdigit():
-			pass
+
+			local = True if args[1] == '-l' else False
+			bound = int(args[2])
+
+			if args[0] == '-p':
+
+				generate_curl_patients(bound, local)
+
+			elif args[0] == '-e':
+
+				generate_curl_episodes(bound, local)
+
+			elif args[0] == '-d':
+
+				generate_curl_doctors(bound, local)
+
+		else:
+			print 'Wrong usage. manual at: python load.py -h'
 
 	else:
 		print 'Wrong usage. manual at: python load.py -h'
