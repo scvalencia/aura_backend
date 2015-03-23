e9bf91e4818e49a2d22cae5a5a662e52f532edd4
cps543 <sc.valencia606@gmail.com>
2015-03-23 11:20:42 -0500

    Supporting file uploading to Soundcloud, and to S3

---
+++ scripts/scala/pom2sbt.scala
@@ -0,0 +1,17 @@
+import scala.xml._
+
+(XML.load("pom.xml") \\ "dependencies") \ "dependency" foreach ((dependency: Node) => {
+  val groupId = (dependency \ "groupId").text
+  val artifactId = (dependency \ "artifactId").text
+  val version = (dependency \ "version").text
+  val scope = (dependency \ "scope").text
+  val classifier = (dependency \ "classifier").text
+  val artifactValName: String = artifactId.replaceAll("[-\\.]", "_")
+
+  print("val %s = \"%s\" %% \"%s\" %% \"%s\"".format(artifactValName, groupId, artifactId, version))
+  scope match {
+    case "" => print("\n")
+    case _ => print(" %% \"%s\"\n".format(scope))
+  }
+  None
+});
